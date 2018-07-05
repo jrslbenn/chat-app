ee951748fadebdf2fb9873bb1172d96a83c600ec
Bennett <jrslbenn@gmail.com>
2018-07-05 16:47:22 -0700

    Add message events

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+var socket = io();
+
+socket.on("connect", function() {
+  console.log("Connected to server.");
+  socket.emit("createMessage", {
+    from: "James",
+    text: "Works for me"
+  })
+});
+
+socket.on("disconnect", function () {
+  console.log("Disconnected from server.");
+});
+
+socket.on("newMessage", function (message) {
+  console.log("New message", message);
+});
